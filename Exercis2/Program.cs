ef0ee166e3b188f1765ef84624eceba63394708e
Roydrigo <lrmagallanj513@gmail.com>
2022-06-09 18:56:01 -0500

    Exercise 3 added

--- Exercis2/Program.cs
+++ Exercis2/Program.cs
@@ -9,12 +9,3 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 string maxNumber = number1 > number2 ? @"the biggest number is: " + number1 : @"The bisggest number is: " + number2;
 Console.WriteLine(maxNumber);
 
-//Exercice 3
-
-Console.WriteLine("Insert the Height of the image: ");
-int height = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Insert the width of the image: ");
-int width = Convert.ToInt32(Console.ReadLine());
-
-string typeOfImage = height > width ? "Your image is a portrait" : "Your image is a landscape :) ";
-Console.WriteLine(typeOfImage);
---
+++ Exercise3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Exercice 3
+
+Console.WriteLine("Insert the Height of the image: ");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Insert the width of the image: ");
+int width = Convert.ToInt32(Console.ReadLine());
+
+string typeOfImage = height > width ? "Your image is a portrait" : "Your image is a landscape :) ";
+Console.WriteLine(typeOfImage);
