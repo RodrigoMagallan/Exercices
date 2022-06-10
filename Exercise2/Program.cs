566e5413d51a809eee5092df49ecd08a91211bde
Roydrigo <lrmagallanj513@gmail.com>
2022-06-09 19:01:22 -0500

    trying to fix names

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Excercice 2 
+
+
+Console.WriteLine("Number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+string maxNumber = number1 > number2 ? @"the biggest number is: " + number1 : @"The bisggest number is: " + number2;
+Console.WriteLine(maxNumber);
+
